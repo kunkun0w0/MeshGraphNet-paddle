3fdf7a7833feccc37a15a7a902750ff23111116a
kunkun0w0 <56219029+kunkun0w0@users.noreply.github.com>
2022-04-25 17:51:11 +0800

    把数据处理放在gpu里做

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import paddle
+
 from dataset import SimpleFlag
 from paddle.io import DataLoader
+from dataset.DataProcess import h5py_to_tensor
 
-test_dataset = SimpleFlag.iterator_dataset(dataset_dir='./data/flag_simple',
+paddle.set_device('gpu')
+
+test_dataset = SimpleFlag.simple_flag(dataset_dir='./data/flag_simple',
                                           split='test',
                                           noise_scale=0.003,
                                           noise_gamma=0.1)
@@ -9,10 +14,11 @@ test_dataset = SimpleFlag.iterator_dataset(dataset_dir='./data/flag_simple',
 
 dataloader = DataLoader(
     test_dataset,
-    num_workers=1,
-    batch_size=4,
+    num_workers=4,
+    batch_size=1,
     drop_last=True)
 
-for i in dataloader:
-    print(i.shape)
+for idx, data in enumerate(dataloader):
+    tmp = h5py_to_tensor(data)
+    print(idx)
 
