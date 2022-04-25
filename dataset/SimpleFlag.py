fa78dbe715b9994b9e8db421e6d24da8fd759205
kunkun0w0 <56219029+kunkun0w0@users.noreply.github.com>
2022-04-25 17:23:14 +0800

    已完成，但是读取速度很慢，待优化

--- dataset/SimpleFlag.py
+++ dataset/SimpleFlag.py
@@ -9,6 +9,27 @@ import h5py
 import common
 
 
+def paddle_gather(x, dim, index):
+    index_shape = index.shape
+    index_flatten = index.flatten()
+    if dim < 0:
+        dim = len(x.shape) + dim
+    nd_index = []
+    for k in range(len(x.shape)):
+        if k == dim:
+            nd_index.append(index_flatten)
+        else:
+            reshape_shape = [1] * len(x.shape)
+            reshape_shape[k] = x.shape[k]
+            x_arrange = paddle.arange(x.shape[k], dtype=index.dtype)
+            x_arrange = x_arrange.reshape(reshape_shape)
+            dim_index = paddle.expand(x_arrange, index_shape).flatten()
+            nd_index.append(dim_index)
+    ind2 = paddle.transpose(paddle.stack(nd_index), [1, 0]).astype("int64")
+    paddle_out = paddle.gather_nd(x, ind2).reshape(index_shape)
+    return paddle_out
+
+
 def add_targets(trajectory, fields, add_history):
     out = {}
     for key in trajectory.keys():
@@ -93,14 +114,25 @@ class iterator_dataset(Dataset):
         node_type = paddle.nn.functional.one_hot(node_type, num_classes=common.NodeType.SIZE)
         node_type = paddle.expand(paddle.unsqueeze(node_type, axis=1),
                                   shape=[node_type.shape[0], velocity.shape[1], node_type.shape[1]])
-        node_features = paddle.concat([velocity, node_type], axis=-1)
+
+        node_features = paddle.concat([velocity, node_type], axis=-1)  # [399, 1579, 3] + [399, 1579, 9]
 
         # construct graph edges
         senders, receivers = common.triangles_to_edges(paddle.to_tensor(np.array(data['cells'])))
-        relative_world_pos = (paddle.gather(data['world_pos'], senders) -
-                              paddle.gather(data['world_pos'], receivers))
-        relative_mesh_pos = (paddle.gather(data['mesh_pos'], senders) -
-                             paddle.gather(data['mesh_pos'], receivers))
+        # senders -> [399, 18168]
+        # receivers -> [399, 18168]
+
+        world_pos = paddle.to_tensor(np.array(data['world_pos']))
+        mesh_pos = paddle.to_tensor(np.array(data['mesh_pos']))
+
+        w_shape = [senders.shape[0], senders.shape[1], 3]
+        m_shape = [senders.shape[0], senders.shape[1], 2]
+
+        relative_world_pos = paddle_gather(x=world_pos, index=senders.unsqueeze(axis=-1).expand(w_shape), dim=1) - \
+                             paddle_gather(x=world_pos, index=receivers.unsqueeze(axis=-1).expand(w_shape), dim=1)
+        relative_mesh_pos = paddle_gather(x=mesh_pos, index=senders.unsqueeze(axis=-1).expand(m_shape), dim=1) - \
+                            paddle_gather(x=mesh_pos, index=receivers.unsqueeze(axis=-1).expand(m_shape), dim=1)
+
         edge_features = paddle.concat([
             relative_world_pos,
             paddle.norm(relative_world_pos, axis=-1, keepdim=True, p=2),
