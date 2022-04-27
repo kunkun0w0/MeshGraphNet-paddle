8bdb5a2717a8c1d22f93b4cdf9ed0f7bb993b7ab
kunkun0w0 <56219029+kunkun0w0@users.noreply.github.com>
2022-04-27 16:04:07 +0800

    修复

--- common.py
+++ common.py
@@ -19,22 +19,23 @@ def triangles_to_edges(faces):
     """
     Computes mesh edges from triangles.
     """
-    # face -> [frames, n_edge, 3]
-    # flag_simple -> [399, 3028, 3]
-    edges = paddle.concat([faces[:, :, 0:2], faces[:, :, 1:3],
-                           paddle.stack([faces[:, :, 2], faces[:, :, 0]], axis=2)], axis=1)
+    # face -> [batch, frames, n_edge, 3]
+    # flag_simple -> [batch, 399, 3028, 3]
+
+    edges = paddle.concat([faces[..., :, 0:2], faces[..., :, 1:3],
+                           paddle.stack([faces[..., :, 2], faces[..., :, 0]], axis=-1)], axis=-2)
 
     # those edges are sometimes duplicated (within the mesh) and sometimes
     # single (at the mesh boundary).
     # sort & pack edges as single tf.int64
-    receivers = paddle.min(edges, axis=2)
-    senders = paddle.max(edges, axis=2)
-    packed_edges = paddle.cast(paddle.stack([senders, receivers], axis=2), paddle.int64)
+    receivers = paddle.min(edges, axis=-1)
+    senders = paddle.max(edges, axis=-1)
+    packed_edges = paddle.cast(paddle.stack([senders, receivers], axis=-1), paddle.int64)
 
     # remove duplicates and unpack
     unique_edges = paddle.cast(paddle.unique(packed_edges, axis=2), paddle.int32)
-    senders, receivers = paddle.unstack(unique_edges, axis=2)
+    senders, receivers = paddle.unstack(unique_edges, axis=-1)
 
     # create two-way connectivity
-    return (paddle.concat([senders, receivers], axis=1),
-            paddle.concat([receivers, senders], axis=1))
+    return (paddle.concat([senders, receivers], axis=-1),
+            paddle.concat([receivers, senders], axis=-1))
