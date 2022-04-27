a533658491296c96538cf10b6c71225bb8188c9e
kunkun0w0 <56219029+kunkun0w0@users.noreply.github.com>
2022-04-27 16:04:31 +0800

    修复

--- dataset/DataProcess.py
+++ dataset/DataProcess.py
@@ -24,27 +24,28 @@ def paddle_gather(x, dim, index):
     return paddle_out
 
 
-def h5py_to_tensor(data):
-    velocity = data['velocity']
-    node_type = data['node_type'][:, 0].squeeze()
-    node_type = paddle.nn.functional.one_hot(node_type, num_classes=common.NodeType.SIZE)
-    node_type = paddle.expand(paddle.unsqueeze(node_type, axis=1),
-                              shape=[node_type.shape[0], velocity.shape[1], node_type.shape[1]])
-
+def h5py_to_tensor(frame):
+    velocity = frame['velocity']
+    node_type = frame['node_type'][:, 0].squeeze_(axis=-1)
+    node_type = paddle.nn.functional.one_hot(node_type, num_classes=common.NodeType.SIZE).unsqueeze_(axis=1)
+    node_type = paddle.expand(node_type, shape=[node_type.shape[0], velocity.shape[1],
+                                                node_type.shape[2], node_type.shape[3]])
     node_features = paddle.concat([velocity, node_type], axis=-1)
 
-    senders, receivers = common.triangles_to_edges(data['cells'])
+    senders, receivers = common.triangles_to_edges(frame['cells'])
+    b, t, edges = senders.shape
 
-    world_pos = data['world_pos']
-    mesh_pos = data['mesh_pos']
+    world_pos = frame['world_pos']
+    mesh_pos = frame['mesh_pos']
+    b, t, nodes, xyz = world_pos.shape
 
-    w_shape = [senders.shape[0], senders.shape[1], 3]
-    m_shape = [senders.shape[0], senders.shape[1], 2]
+    w_shape = [b, t, edges, 3]
+    m_shape = [b, t, edges, 2]
 
-    relative_world_pos = paddle_gather(x=world_pos, index=senders.unsqueeze(axis=-1).expand(w_shape), dim=1) - \
-                         paddle_gather(x=world_pos, index=receivers.unsqueeze(axis=-1).expand(w_shape), dim=1)
-    relative_mesh_pos = paddle_gather(x=mesh_pos, index=senders.unsqueeze(axis=-1).expand(m_shape), dim=1) - \
-                        paddle_gather(x=mesh_pos, index=receivers.unsqueeze(axis=-1).expand(m_shape), dim=1)
+    relative_world_pos = paddle_gather(x=world_pos, index=senders.unsqueeze(axis=-1).expand(w_shape), dim=2) - \
+                         paddle_gather(x=world_pos, index=receivers.unsqueeze(axis=-1).expand(w_shape), dim=2)
+    relative_mesh_pos = paddle_gather(x=mesh_pos, index=senders.unsqueeze(axis=-1).expand(m_shape), dim=2) - \
+                        paddle_gather(x=mesh_pos, index=receivers.unsqueeze(axis=-1).expand(m_shape), dim=2)
 
     edge_features = paddle.concat([
         relative_world_pos,
@@ -52,4 +53,4 @@ def h5py_to_tensor(data):
         relative_mesh_pos,
         paddle.norm(relative_mesh_pos, axis=-1, keepdim=True, p=2)], axis=-1)
 
-    return node_features, edge_features, senders, receivers, data
+    return node_features, edge_features, senders, receivers, frame
